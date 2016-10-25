cf41eebc81f5102046cf0fdbc6ebcb1ea9b6d122
Jessica Dembe <jmdembe@gmail.com>
2016-10-25 15:14:17 -0400

    Created the fuction to split the string

--- js/code.js
+++ js/code.js
@@ -1,8 +1,11 @@
-function wordCounting ({
+var newString = 'She sells seashells by the seashore';
+
+function wordCounting (myStr) {
+    var separator = ' ';
+    var wordCount = myStr.split(separator);
 
 
+    return wordCount;
+  };
 
-
-
-
-});
+console.log(wordCounting(newString));
