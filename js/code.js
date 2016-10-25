0d93c361ec04c2a2a3a5089cf883fe5d8f42aa59
Jessica Dembe <jmdembe@gmail.com>
2016-10-25 17:40:44 -0400

    Finished code to count frequency of words

--- js/code.js
+++ js/code.js
@@ -1,15 +1,23 @@
-var newString = 'She sells seashells by the seashore';
+var newString = 'She sells seashells by the seashore seashells';
 
-function wordCounting (myStr) {
-    var frequency = {};
-    var separator = ' ';
+function wordSplitter (myStr) {
     var wordArray = myStr.split(' ');
+    var frequency = {};
 
-    wordArray.forEach(function wordFrequency(each) {
-          console.log(each);
+    wordArray.forEach(function count (word){
+        // frequency[word]=1;
+
+        console.log("Frequency Word",word, frequency[word]);
+
+        if (frequency[word] === undefined) {
+            frequency[word] = 1;
+        } else {
+            frequency[word]++;
+        }
+
     });
+    return frequency;
+};
 
-    return wordArray;
-  };
-
-console.log(wordCounting(newString));
+var result=wordSplitter(newString);
+console.log(result);
